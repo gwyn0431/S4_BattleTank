a0e3ee60ac250c8e0a44e122ad73e393a91ea6fb
Home PC <gwyn0431@gmail.com>
2017-11-23 19:02:14 +0300

    Configured LogOut at BeginPlay, reports if we found a ControlledTank or not.

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,6 +3,20 @@
 #include "TankPlayerController.h"
 #include "BattleTank.h"
 
+void  ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	ControlledTank = GetControlledTank();
+	if (ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing %s"), *(ControlledTank->GetName()));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("PlayerController not possessing a tank."));
+	}
+	
+}
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
