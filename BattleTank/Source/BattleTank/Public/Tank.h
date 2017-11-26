d7ae3a9928f995efe87cfbe9669d39276a3c03b1
Home PC <gwyn0431@gmail.com>
2017-11-26 16:19:25 +0300

    Added TankBarrel class and refactored code for matching Barrel being UTankBarrel instead of UStaticMesh.

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -7,6 +7,7 @@
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h" // Paste new includes ABOVE it
 
+class UTankBarrel;
 
 UCLASS()
 class BATTLETANK_API ATank : public APawn
@@ -17,7 +18,7 @@ public:
 	void AimAt(FVector HitLocation);
 	
 	UFUNCTION(BlueprintCallable, Category = Setup)
-	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
+	void SetBarrelReference(UTankBarrel* BarrelToSet);
 
 protected:
 	UTankAimingComponent* TankAimingComponent = nullptr;
